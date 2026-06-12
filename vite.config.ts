768e655fbf2d0cb4080feb7e22c299bf3f0cd63a
griffin121 <46709380+griffin121@users.noreply.github.com>
2026-06-12 15:50:33 -0500

    Change base URL for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Set base to your GitHub Pages repo name, e.g. '/world-cup-gtj/'
-// Use '/' if deploying to a custom domain or username.github.io root.
+// Set base to your GitHub Pages repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/world-cup-gtj/'
+  base: '/1/'
 });
